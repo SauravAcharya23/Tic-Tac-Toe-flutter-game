8c547baa34ae481f2a1a4cdf2544bdbe3bf7ae8d
Saurav Acharya <sauravacharya236@gmail.com>
2023-02-09 11:32:18 +0545

    Fully Functional

--- lib/view/game_screen.dart
+++ lib/view/game_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tic_tac_toe_game/constants/colors.dart';
 
 class GameScreen extends StatefulWidget {
@@ -9,6 +10,13 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+
+  bool oTurn = true;
+  List<String> displayXO = ['','','','','','','','','',];
+  String resultDeclaration = '';
+  int filledBoxes = 0;
+  bool winnerFound = false;
+  List<int> matchedIndexes = [];
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +30,16 @@ class _GameScreenState extends State<GameScreen> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Player 1', style: TextStyle(color: MainColor.secondaryColor),),
-                    Text('Player 2', style: TextStyle(color: MainColor.secondaryColor),)
+                    Text('Player O', style: TextStyle(color: MainColor.secondaryColor),),
+                    Text('Player X', style: TextStyle(color: MainColor.secondaryColor),)
                   ],
                 ),
                 const SizedBox(
-                  height: 150,
+                  height: 120,
+                ),
+                resultDeclaration == '' ? const SizedBox(width: 0,) : Text(resultDeclaration.toString(), style: TextStyle(color: MainColor.secondaryColor),),
+                const SizedBox(
+                  height: 20,
                 ),
                 Expanded(
                   child: GridView.builder(
@@ -40,6 +52,7 @@ class _GameScreenState extends State<GameScreen> {
                     itemBuilder: (context, index) {
                       return GestureDetector(
                         onTap: () {
+                          _onTapped(index);
                         },
                         child: Container(
                           decoration: BoxDecoration(
@@ -48,19 +61,149 @@ class _GameScreenState extends State<GameScreen> {
                               width: 5,
                               color: MainColor.primaryColor
                             ),
-                          color: MainColor.secondaryColor
+                          color: matchedIndexes.contains(index)? Colors.red : MainColor.secondaryColor
                         ),
-                        child: const Center(child: Text('0', style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),)),
+                        child: Center(child: Text(displayXO[index], style: TextStyle(fontSize: 32, fontWeight: FontWeight.w800, color: MainColor.primaryColor),)),
                       ),
                     );
                   },
                 ),
               ),
+              
             ],
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(backgroundColor: MainColor.secondaryColor, onPressed: (){}, child: const Icon(Icons.start_rounded,)),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: MainColor.secondaryColor, 
+        onPressed: (){
+          _clearBoard();
+        }, 
+        child: const Icon(Icons.start_rounded,)
+      ),
     );
+  }
+
+  void _clearBoard(){
+    setState(() {
+      for(int i = 0; i < 9; i++){
+        displayXO[i] = '';
+      }
+      resultDeclaration = '';
+      matchedIndexes = [];
+      oTurn = true;
+    });
+    filledBoxes = 0;
+  }
+
+  void _onTapped(int index){
+    if(resultDeclaration == ''){
+      setState(() {
+        if(oTurn && displayXO[index] == ''){
+          displayXO[index] = 'O';
+          filledBoxes++;
+        }else if(!oTurn && displayXO[index] == ''){
+          displayXO[index] = 'X';
+          filledBoxes++;
+        }
+        oTurn = !oTurn;
+        _checkWinner();
+      });
+    }
+    
+  }
+
+  void _checkWinner(){
+    if(displayXO[0] == displayXO[1] && displayXO[0] == displayXO[2] && displayXO[0] != ''){
+      setState(() {
+        resultDeclaration = 'Player'+displayXO[0] +'Wins!';
+        winnerFound = true;
+        matchedIndexes.addAll([0, 1, 2]);
+      });
+      
+    }
+    // check 2nd row
+    if (displayXO[3] == displayXO[4] &&
+        displayXO[3] == displayXO[5] &&
+        displayXO[3] != '') {
+      setState(() {
+        resultDeclaration = 'Player ' + displayXO[3] + ' Wins!';
+        winnerFound = true;
+        matchedIndexes.addAll([3, 4, 5]);
+      });
+    }
+
+    // check 3rd row
+    if (displayXO[6] == displayXO[7] &&
+        displayXO[6] == displayXO[8] &&
+        displayXO[6] != '') {
+      setState(() {
+        resultDeclaration = 'Player ' + displayXO[6] + ' Wins!';
+        winnerFound = true;
+        matchedIndexes.addAll([6, 7, 8]);
+      });
+    }
+
+    // check 1st column
+    if (displayXO[0] == displayXO[3] &&
+        displayXO[0] == displayXO[6] &&
+        displayXO[0] != '') {
+      setState(() {
+        resultDeclaration = 'Player ' + displayXO[0] + ' Wins!';
+        winnerFound = true;
+        matchedIndexes.addAll([0, 3, 6]);
+      });
+    }
+
+    // check 2nd column
+    if (displayXO[1] == displayXO[4] &&
+        displayXO[1] == displayXO[7] &&
+        displayXO[1] != '') {
+      setState(() {
+        resultDeclaration = 'Player ' + displayXO[1] + ' Wins!';
+        winnerFound = true;
+        matchedIndexes.addAll([1, 4, 7]);
+      });
+    }
+
+    // check 3rd column
+    if (displayXO[2] == displayXO[5] &&
+        displayXO[2] == displayXO[8] &&
+        displayXO[2] != '') {
+      setState(() {
+        resultDeclaration = 'Player ' + displayXO[2] + ' Wins!';
+        winnerFound = true;
+        matchedIndexes.addAll([2, 5, 8]);
+      });
+    }
+
+    // check diagonal
+    if (displayXO[0] == displayXO[4] &&
+        displayXO[0] == displayXO[8] &&
+        displayXO[0] != '') {
+      setState(() {
+        resultDeclaration = 'Player ' + displayXO[0] + ' Wins!';
+        winnerFound = true;
+        matchedIndexes.addAll([0, 4, 8]);
+      });
+    }
+
+    // check diagonal
+    if (displayXO[6] == displayXO[4] &&
+        displayXO[6] == displayXO[2] &&
+        displayXO[6] != '') {
+      setState(() {
+        resultDeclaration = 'Player ' + displayXO[6] + ' Wins!';
+        winnerFound = true;
+        matchedIndexes.addAll([6, 4, 2]);
+      });
+    }
+
+    if(!winnerFound && filledBoxes == 9){
+      setState(() {
+        resultDeclaration = 'Nobody wins!';
+      });
+    }
+    
   }
 }
